ed15319f7b4b7ff086df2eb4ecd8534f96bfda5e
Luana <lualuna182@gmail.com>
2021-03-13 10:58:41 -0300

    Versão estilizada

--- src/components/EnvioMensagem.js
+++ src/components/EnvioMensagem.js
@@ -1,4 +1,47 @@
 import React from 'react'
+import styled from "styled-components"
+
+
+const Container = styled.div`
+width: 50%;
+height: 50%;
+background-color: white;
+border-radius: 20px;
+
+display: flex;
+flex-direction: column;
+justify-items:flex-end;
+align-items:center;
+backdrop-filter: blur(20px);
+` 
+
+const ContainerInput = styled.div`
+width: 95%;
+height: 10%;
+display: flex;
+justify-content: center;
+align-items:center;
+margin: 2%;`
+
+const ContainerMensagem = styled.div`
+width: 95%;
+height: 90%;
+overflow: auto;
+scroll-behavior: unset;
+`
+
+const StyledInput = styled.input`
+width: 60%;
+border: none;
+border-bottom: 1px solid black;
+padding: 1%;` 
+
+const InputUser = styled.input`
+width: 30%;
+border: none;
+border-bottom: 1px solid black;
+padding: 1%;
+margin-right: 1%;`
 
 
 export default class EnvioMensagem extends React.Component {
@@ -27,7 +70,7 @@ export default class EnvioMensagem extends React.Component {
         };
 
         const novaListaConteudo = [...this.state.mensagem, novaMensagem];
-        this.setState({ mensagem: novaListaConteudo});
+        this.setState({ mensagem: novaListaConteudo, inputValorconteudo: ''});
 
         console.log(this.criarconteudo)
 
@@ -46,22 +89,27 @@ export default class EnvioMensagem extends React.Component {
         console.log(componenteMensagem)
 
         return (
-            <div>
+            <Container>
+                <ContainerMensagem>
                 <div>{componenteMensagem}</div>
-                <input
+                </ContainerMensagem>
+
+                <ContainerInput>
+                <InputUser
                 placeholder={'Usuário'} 
                 onChange={this.pegarInputUsuario} 
                 value={this.state.inputUsuarioValor} 
                 /> 
                 
-                <input 
+                <StyledInput 
                 placeholder={'Digite sua mensagem'} 
                 onChange={this.pegarInputconteudo}
                 value={this.state.inputValorconteudo}
                 />
 
                 <button onClick={this.criarconteudo}>Enviar</button>
-            </div>
+                </ContainerInput>
+            </Container>
         );
     };
 };
